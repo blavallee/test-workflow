bc435b33eb66ec7a3a53c6c484b2e42a2fa1b7f0
Benoit Lavallee <blavallee@coveo.com>
2021-01-21 14:45:53 -0500

    Test again

--- class1.h
+++ class1.h
@@ -6,25 +6,23 @@
 
 
 
-
-
-    #include <file1.h>
+   #include <file1.h>
  
-namespace class1 {
+ namespace class1 {
  
 class class1 {
-         public:
-   class1() {};
+        public:
+       class1() {};
    int                              method1();
            void    method2();
     void meth3();
-      void         method4();
-	   int method5();
-	int                     method6();
+       void         method4();
+    int method5();
+    int                     method6();
   
-};
+ };
 
-} 
-
-        #endif //CLASS1_H
+ } 
+ 
+     #endif //CLASS1_H
 
