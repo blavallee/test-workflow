1297246b744c89f8df30d9845617bbab9863f9aa
blavallee <blavallee@coveo.com>
2021-01-22 10:53:07 -0500

    !!!

--- class1.h
+++ class1.h
@@ -6,15 +6,15 @@
 
 
  
-  #include <file1.h>
-    namespace class1 {
+   #include <file1.h>
+     namespace class1 {
 class  class1 {
-       public:
-       class1() {};
+        public:
+        class1() {};
   int                               method1();
-     void    method2();
+      void    method2();
  void meth3();
-       void         method4();
+        void         method4();
  int method5();
   int                     method6();
  
