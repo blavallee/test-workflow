416c731c35cc142c4e228c63f27bbd07b1fadcc3
blavallee <blavallee@coveo.com>
2021-01-22 10:32:14 -0500

    modify files

--- class1.h
+++ class1.h
@@ -6,16 +6,16 @@
 
 
 
- #include <file1.h>
-   namespace class1 {
+  #include <file1.h>
+    namespace class1 {
 class  class1 {
-      public:
-      class1() {};
- int                              method1();
+       public:
+       class1() {};
+  int                              method1();
      void    method2();
   void meth3();
-      void         method4();
- int method5();
+       void         method4();
+  int method5();
    int                     method6();
  
  };
