111aa1fda770b4ec44ad4a29c434bc44cc25c2ee
blavallee <blavallee@coveo.com>
2021-01-22 11:23:41 -0500

    context

--- class1.h
+++ class1.h
@@ -11,10 +11,10 @@
 class class1 {
       public:
       class1() {};
-int                               method1();
-    void    method2();
- void meth3();
-        void         method4();
+int                                method1();
+     void    method2();
+  void meth3();
+         void         method4();
  int method5();
   int                     method6();
   
