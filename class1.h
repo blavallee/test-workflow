97d0c30f4f345faf588c8d67b64038408a222a53
Benoit Lavallee <blavallee@coveo.com>
2021-01-25 16:55:19 -0500

    New diff

--- class1.h
+++ class1.h
@@ -3,21 +3,21 @@
 
 #pragma once
 
-
- #include <file1.h>
-  namespace class1 {
- class class1 {
-  public:
-     class1() {};
-  int                                method1();
-       void    method2();
-     void meth3();
-           void         method4();
+#include <file1.h>
+namespace class1 {
+class class1
+{
+public:
+    class1(){};
+    int                      method1();
+    void                     method2();
+    void                     meth3();
+    void                     method4();
     int method5();
      int                     method6();
   
   };
- 
-} 
-    #endif //CLASS1_H
+
+  }        // namespace class1
+#endif //CLASS1_H
 
